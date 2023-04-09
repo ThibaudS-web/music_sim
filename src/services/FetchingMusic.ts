fb8fa9b596d22b1be6ea47b85779c334e610750a
ThibaudS <thibaud.devpro@gmail.com>
2023-04-09 21:35:39 +0200

    change url fetch

--- src/services/FetchingMusic.ts
+++ src/services/FetchingMusic.ts
@@ -7,7 +7,7 @@ class FetchingMusic {
 	async getTracksByGenre(genre: string): Promise<Track[]> {
 		try {
 			let data: Track[]
-			const result = await fetch(`${this.baseURL}/${genre}.json`)
+			const result = await fetch(`/music_sim/${genre}.json`)
 			data = await result.json()
 			return data
 		} catch {
