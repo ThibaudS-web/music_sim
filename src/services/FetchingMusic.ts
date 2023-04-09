28d43f400341bf72ecf485872df8248c516b408c
ThibaudS <thibaud.devpro@gmail.com>
2023-04-09 21:38:59 +0200

    change url fetch

--- src/services/FetchingMusic.ts
+++ src/services/FetchingMusic.ts
@@ -2,8 +2,6 @@ import Track from "../models/Track"
 
 /**  @description Retrieving mocked data.*/
 class FetchingMusic {
-	baseURL = import.meta.env.DEV ? "http://localhost:3000/music_sim" : "https://thibauds-web.github.io/music_sim"
-
 	async getTracksByGenre(genre: string): Promise<Track[]> {
 		try {
 			let data: Track[]
